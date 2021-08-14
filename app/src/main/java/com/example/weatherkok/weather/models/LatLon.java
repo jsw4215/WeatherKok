8034c0215c7fd6e2a7bbaf76632ffd08fc9defe9
jsw4215 <jsw4215@philosys.com>
2021-08-14 12:14:41 +0900

    fourth

---
+++ app/src/main/java/com/example/weatherkok/weather/models/LatLon.java
@@ -0,0 +1,3 @@
+package com.example.weatherkok.weather.models;public class LatLon {
+private static final String TAG = LatLon.class.getSimpleName();
+}
---
+++ app/src/main/java/com/example/weatherkok/weather/models/midTemp/MidTempResponse.java
@@ -0,0 +1,3 @@
+package com.example.weatherkok.weather.models.midTemp;public class MidTempResponse {
+private static final String TAG = MidTempResponse.class.getSimpleName();
+}
